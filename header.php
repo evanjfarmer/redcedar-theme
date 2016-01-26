686f29dc09f5774a72172d99b0749c4254fa4c71
evanjfarmer <evanjfarmer@gmail.com>
2016-01-26 11:22:48 -0500

    changed woocommerce.css to make product listings more responsive, too app.css out of header, and added links to home page headings

--- header.php
+++ header.php
@@ -18,7 +18,6 @@
 
 <?php wp_head(); ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="foundation/css/app.css" />
 </head>
 
 <body <?php body_class(); ?>>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -13,21 +13,22 @@
 <div class="entry-content">
 	<div class="row">
   		<div class="home large-6 columns">
-  			<a href="#"><h3>MIDWIFERY SERVICES</h3></a>
+  			<a href="/midwifery-services"><h3>MIDWIFERY SERVICES</h3></a>
   			<p>Our home birth care is holistic and family centered. We promote active birthing, minimal interventions, and shared decision making throughout the childbearing year. We would love to sit down with you and talk about your plans</p>
   		</div>
   		<div class="home large-6 columns">
-  			<a href="#"><h3>DOULA SERVICES</h3></a>
+  			<a href="/doula-services"><h3>DOULA SERVICES</h3></a>
   			<p>It is our goal to provide each client with care that is respectful and centered around their choices and needs. Whether birthing at home or in the hospital, we are committed to providing you with evidence based information so that you can take lead in the decision making.</p>
   		</div>
   	</div>
 	<hr />
-	<a href="#"><h3 class="home">BOTANICALS SHOP</h3></a><br>
-		<ul class="products">
-			<?php
-			echo do_shortcode( '[recent_products per_page="3" columns="3"]' );
-			?>
-		</ul><!--/.products-->
+	<a href="/botanicals"><h3 class="home">BOTANICALS SHOP</h3></a><br>
+		<div class="row">
+			<ul class="products small-12 large-12 columns">
+				<?php
+				echo do_shortcode( '[recent_products per_page="3" columns="3"]' );
+				?>
+			</ul><!--/.products-->
 	<hr />
 	<div class="row center">
 		<div class="small-12 medium-6 large-4 columns">
